22fec9c121b80edc22ae92bccbffad448b559c56
Keith Taylor <KTSnowy@outlook.com>
2022-10-13 20:08:47 -0300

    [Update]: Commit unfinished Lexer code

--- src/OtterkitCompiler.cs
+++ src/OtterkitCompiler.cs
@@ -3,6 +3,7 @@
 public static class OtterkitCompiler
 {
     private static string fileName = "main.cob";
+    private static string sourceFormat = "fixed";
     public static void Main(string[] args)
     {
         if (args.Length <= 1)
@@ -10,30 +11,62 @@ public static class OtterkitCompiler
             DisplayHelpMessage();
             Environment.Exit(0);
         }
+
         ProcessArguments(args);
-        Console.WriteLine(fileName);
+        List<string> sourceLines = ReadAndProcessFile(fileName, sourceFormat);
+        OtterkitLexer.Tokenize(sourceLines);
     }
 
     private static void ProcessArguments(string[] args)
     {
         // Index is always +1 the true index:
-        // This makes it easier to get the next item
+        // This makes it easier to get the next item,
         // after the command line argument, like the filename.
         var index = 0;
         foreach (string argument in args)
         {
             index += 1;
-            if (argument == "help")
+            switch (argument)
             {
-                DisplayHelpMessage();
-                Environment.Exit(0);
+                // -H meaning Help
+                case "-H":
+                    DisplayHelpMessage();
+                    Environment.Exit(0);
+                    break;
+                // -F meaning File
+                case "-F":
+                    fileName = args[index];
+                    break;
+                // --Fixed meaning Fixed Format
+                case "--Fixed":
+                    sourceFormat = "fixed";
+                    break;
+                // --Free meaning Free Format
+                case "--Free":
+                    sourceFormat = "free";
+                    break;
+                default: break;
             }
-            if (argument == "file")
-            {
-                fileName = args[index];
-            }
+
         }
         return;
+    }
+    private static List<string> ReadAndProcessFile(string fileName, string sourceFormat)
+    {
+        string[] readLines = File.ReadAllLines(fileName);
+        List<string> processedLines = new();
+        foreach (string line in readLines)
+        {
+            if (line.Length >= 6 && sourceFormat == "fixed")
+            {
+                processedLines.Add(line.Substring(6));
+            }
+            else
+            {
+                processedLines.Add(line);
+            }
+        }
+        return processedLines;
     }
 
     private static void DisplayHelpMessage()
--- src/OtterkitLexer.cs
+++ src/OtterkitLexer.cs
@@ -1,9 +1,20 @@
-namespace Otterkit;
+using System.Text.RegularExpressions;
 
+namespace Otterkit;
 public static class OtterkitLexer
 {
-    public static void Lexer(string[] sourceLines)
+    public static void Tokenize(List<string> sourceLines)
     {
-        
+        int lineNumber = 0;
+        string pattern = @"\w+(-[\w0-9]+)*|\w+|[^\w\s]|(\r\n|\r|\n)";
+        foreach (string line in sourceLines)
+        {
+            lineNumber += 1;
+            foreach (Match match in Regex.Matches(line, pattern, RegexOptions.IgnoreCase))
+            {
+                Console.WriteLine("<Line {0}: Column {1}>: {2}", lineNumber, match.Index, match.Value);
+            }
+
+        }
     }
 }

67502e9e97994ba3bb9c5bab2254c14afc671f57
Keith Taylor <KTSnowy@outlook.com>
2022-10-14 17:46:08 -0300

    [Fix]: Fixed extra space inside regex

--- src/OtterkitLexer.cs
+++ src/OtterkitLexer.cs
@@ -6,7 +6,7 @@ public static class OtterkitLexer
     public static void Tokenize(List<string> sourceLines)
     {
         int lineNumber = 0;
-        string pattern = "\\w+(-[\\w0-9]+)*|(^-\\s* \"|\")(.*?)(\"|(\r\n|\r|\n)|$)|\\w+|[^\\w\\s]|(\r\n|\r|\n)";
+        string pattern = "\\w+(-[\\w0-9]+)*|(^-\\s*\"|\")(.*?)(\"|(\r\n|\r|\n)|$)|\\w+|[^\\w\\s]|(\r\n|\r|\n)";
         foreach (string line in sourceLines)
         {
             lineNumber += 1;

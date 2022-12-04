1c1739c747118350cbd32e8dc3cc0fabf2c1edd6
Keith Taylor <KTSnowy@outlook.com>
2022-12-03 22:06:56 -0300

    [Update]: Commit unfinished External changes

--- src/OtterkitExternal.cs
+++ src/OtterkitExternal.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace OtterkitLibrary;
 
 public sealed class External
@@ -5,12 +7,13 @@ public sealed class External
     private static List<Memory<byte>> ExternalSpace = new();
     private static List<string> ExternalDataItems = new();
     private static List<int> ExternalItemsLength = new();
+    private static List<string> ExternalDefaultValues = new();
 
-    public static Memory<byte> Resolver(string dataItemName, int bytes)
+    public static Memory<byte> Resolver(string dataItemName, int bytes, string defaultValue)
     {
         int IndexOfDataItem = ExternalDataItems.IndexOf(dataItemName);
 
-        if (IndexOfDataItem > -1 && ExternalItemsLength[IndexOfDataItem] == bytes)
+        if (IndexOfDataItem > -1 && ExternalItemsLength[IndexOfDataItem] == bytes && ExternalDefaultValues[IndexOfDataItem] == defaultValue)
             return ExternalSpace[IndexOfDataItem];
 
         if (IndexOfDataItem == -1)
@@ -18,8 +21,10 @@ public sealed class External
             ExternalSpace.Add(new byte[bytes]);
             ExternalDataItems.Add(dataItemName);
             ExternalItemsLength.Add(bytes);
+            ExternalDefaultValues.Add(defaultValue);
+            Encoding.UTF8.GetBytes(defaultValue).CopyTo(ExternalSpace[ExternalSpace.Count].Span);
 
-            return Resolver(dataItemName, bytes);
+            return Resolver(dataItemName, bytes, defaultValue);
         }
 
         throw new ArgumentException("Name of external data item exists, but the byte length does not match");

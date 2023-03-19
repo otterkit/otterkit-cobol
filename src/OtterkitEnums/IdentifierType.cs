0ff05592e6437b7f582f48768ab5fc6f2091d344
Keith Taylor <KTSnowy@outlook.com>
2023-03-19 08:37:45 -0300

    [Update]: Refactor IdentifierType enum to use left shifts
    This improves readability and maintainability. It's easier to add +1 to the next shift.

--- src/OtterkitEnums/IdentifierType.cs
+++ src/OtterkitEnums/IdentifierType.cs
@@ -1,21 +1,21 @@
 namespace Otterkit;
 
 [Flags] 
-public enum IdentifierType : short
+public enum IdentifierType
 {
     None = 0,
-    Function = 1,
-    ReferenceMod = 2,
-    MethodInvocation = 4,
-    ObjectView = 8,
-    ExceptionObject = 16,
-    NullObject = 34,
-    Self = 64,
-    Super = 128,
-    NullAddress = 256,
-    DataAddress = 512,
-    FunctionAddress = 1024,
-    ProgramAddress = 2048,
-    LinageCounter = 4096,
-    ReportCounter = 8192,
+    Self = 1,
+    Super = 1 << 1,
+    Function = 1 << 2,
+    NullObject = 1 << 3,
+    ObjectView = 1 << 4,
+    DataAddress = 1 << 5,
+    NullAddress = 1 << 6,
+    ReferenceMod = 1 << 7,
+    LinageCounter = 1 << 8,
+    ReportCounter = 1 << 9,
+    ProgramAddress = 1 << 10,
+    FunctionAddress = 1 << 11,
+    ExceptionObject = 1 << 12,
+    MethodInvocation = 1 << 13,
 }

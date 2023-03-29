25ab2b267dd29bcc706a5b490b4dc88b04e2f3c8
Keith Taylor <KTSnowy@outlook.com>
2023-03-29 11:57:52 -0300

    [Update]: Add all signatures into a single folder

--- src/OtterkitSymbolTable/Signatures/CallableSignature.cs
+++ src/OtterkitSymbolTable/Signatures/CallableSignature.cs
@@ -2,6 +2,8 @@ namespace Otterkit;
 
 public class CallableSignature : AbstractSignature
 {
+    public LocalReferences<DataSignature> DataDefinitions = new();
+    public List<RepositorySignature> Repository = new();
     public List<DataSignature> Parameters = new();
     public List<bool> IsOptional = new();
     public List<bool> IsByRef = new();

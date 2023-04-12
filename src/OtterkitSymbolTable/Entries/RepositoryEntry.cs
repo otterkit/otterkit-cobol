8814e5a48cf4bfed07f012472bb022b79744b489
Keith Taylor <KTSnowy@outlook.com>
2023-04-12 18:22:09 -0300

    [Update]: Refactor RepositoryDefinition

---
+++ src/OtterkitSymbolTable/Entries/RepositoryEntry.cs
@@ -0,0 +1,11 @@
+namespace Otterkit;
+
+public class RepositoryEntry : AbstractEntry
+{
+    public SourceUnit SourceType;
+    public string Expands = string.Empty;
+    public List<string>? Using;
+
+    public RepositoryEntry(Token identifier, EntryType entryType) 
+        : base(identifier, entryType) { }
+}

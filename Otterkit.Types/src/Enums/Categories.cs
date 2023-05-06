c64506500b2d0aa14fc98663501496965831f762
Keith Taylor <KTSnowy@outlook.com>
2023-05-06 15:44:14 -0300

    [Update]: Create elementary data category enum

---
+++ Otterkit.Types/src/Enums/Categories.cs
@@ -0,0 +1,21 @@
+namespace Otterkit.Types;
+
+[Flags]
+public enum Categories
+{
+    Error,
+    Alphabetic,
+    Alphanumeric            = 1 << 1,
+    AlphanumericEdited      = 1 << 2,
+    Boolean                 = 1 << 3,
+    Index                   = 1 << 4,
+    MessageTag              = 1 << 5,
+    National                = 1 << 6,
+    NationalEdited          = 1 << 7,
+    Numeric                 = 1 << 8,
+    NumericEdited           = 1 << 9,
+    ObjectReference         = 1 << 10,
+    DataPointer             = 1 << 11,
+    FunctionPointer         = 1 << 12,
+    ProgramPointer          = 1 << 13
+}

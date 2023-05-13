c95fbe07822cf275dc104c514b7afa59a8209069
Keith Taylor <KTSnowy@outlook.com>
2023-05-13 02:06:01 -0300

    [Refactor]: Split LocalNames into two classes (other names don't need a list)

--- Otterkit.Types/src/Symbols/LocalNames.cs
+++ Otterkit.Types/src/Symbols/LocalNames.cs
@@ -5,68 +5,28 @@ namespace Otterkit.Types;
 
 public sealed class LocalNames<TValue> where TValue: notnull
 {
-    private readonly Dictionary<string, List<TValue>> NameLookup = new(StringComparer.OrdinalIgnoreCase);
+    private readonly Dictionary<string, TValue> NameLookup = new(StringComparer.OrdinalIgnoreCase);
 
-    public void AddEntry(Token entry, TValue localEntry)
+    public bool TryAddName(Token name, TValue localName)
     {
-        ref var entries = ref CollectionsMarshal.GetValueRefOrAddDefault(NameLookup, entry.Value, out var exists);
+        ref var names = ref CollectionsMarshal.GetValueRefOrAddDefault(NameLookup, name.Value, out var exists);
 
         if (!exists)
         {
-            entries = new(1);
-            entries.Add(localEntry);
+            names = localName;
+            
+            return true;
         }
-
-        if (exists && entries is not null) entries.Add(localEntry);
-
-        if (exists && entries is null)
-        {
-            throw new ArgumentException("Local entry exists but value was null in the NameLookup dictionary", nameof(entry));
-        }
-    }
-
-    public bool EntryExists(Token entry)
-    {
-        ref var entries = ref CollectionsMarshal.GetValueRefOrNullRef(NameLookup, entry.Value);
-
-        if (!Unsafe.IsNullRef(ref entries)) return true;
 
         return false;
     }
 
-    public (bool, bool) HasUniqueEntry(Token entry)
+    public bool NameExists(Token name)
     {
-        ref var entries = ref CollectionsMarshal.GetValueRefOrNullRef(NameLookup, entry.Value);
+        ref var names = ref CollectionsMarshal.GetValueRefOrNullRef(NameLookup, name.Value);
 
-        if (!Unsafe.IsNullRef(ref entries) && entries is not null)
-        {
-            return (true, entries.Count == 1);
-        }
+        if (!Unsafe.IsNullRef(ref names)) return true;
 
-        return (false, false);
-    }
-
-    public List<TValue> EntriesList(Token entry)
-    {
-        ref var entries = ref CollectionsMarshal.GetValueRefOrNullRef(NameLookup, entry.Value);
-
-        if (!Unsafe.IsNullRef(ref entries) && entries is not null)
-        {
-            return entries;
-        }
-
-        throw new ArgumentOutOfRangeException(nameof(entry), "Local entry does not exist in the NameLookup dictionary");
-    }
-
-    public TValue UniqueEntry(Token entry)
-    {
-        ref var entries = ref CollectionsMarshal.GetValueRefOrNullRef(NameLookup, entry.Value);
-
-        if (!Unsafe.IsNullRef(ref entries) && entries is not null)
-        {
-            return entries[0];
-        }
-
-        throw new ArgumentOutOfRangeException(nameof(entry), "Local entry does not exist in the NameLookup dictionary");
+        return false;
     }
 }

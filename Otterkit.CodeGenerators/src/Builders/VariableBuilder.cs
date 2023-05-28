2ec01c418b3a5b912a002fa3b859269458afbf85
KTSnowy <KTSnowy@outlook.com>
2023-05-27 22:45:25 -0300

    [Update]: Merge BuildDataEntry and Build77 into one method

--- Otterkit.CodeGenerators/src/Builders/VariableBuilder.cs
+++ Otterkit.CodeGenerators/src/Builders/VariableBuilder.cs
@@ -45,9 +45,9 @@ public class VariableBuilder
 
         var variable = callable.DataNames.FetchUnique(token);
 
-        if (variable.LevelNumber is 77)
+        if (variable.LevelNumber is 77 or 1)
         {
-            Build77(variable, token);
+            BuildVariable(variable, token);
             ExportVariable();
             return;
         }
@@ -58,147 +58,6 @@ public class VariableBuilder
             ExportVariable();
             return;
         }
-
-        if (variable.LevelNumber is 1)
-        {
-            BuildDataEntry();
-            ExportVariable();
-            return;
-        }
-    }
-
-    private void BuildDataEntry()
-    {
-        bool isElementary = false;
-        bool isExternal = false;
-        bool isSigned = false;
-        bool isLevelOne = (LevelNumber.Equals("01") || LevelNumber.Equals("1"));
-
-        while (!CurrentEquals("."))
-        {
-            if (CurrentEquals("PIC") || CurrentEquals("PICTURE"))
-            {
-                isElementary = true;
-                Continue(1);
-                if (CurrentEquals("IS")) Continue(1);
-
-                DataType = Current().Value;
-                if (CurrentEquals("S9")) isSigned = true;
-
-                Continue(2);
-
-                Length = int.Parse(Current().Value);
-
-                if ((DataType.Equals("9") || DataType.Equals("S9")) && PeekEquals(2, "V9"))
-                    FractionalLength = int.Parse(Peek(4).Value);
-            }
-
-            if ((CurrentEquals("IS") && PeekEquals(1, "EXTERNAL")) || CurrentEquals("EXTERNAL"))
-            {
-                string externalizedName = Identifier;
-                isExternal = true;
-
-                if (CurrentEquals("IS"))
-                    Continue(1);
-
-                if (PeekEquals(1, "EXTERNAL"))
-                {
-                    Continue(2);
-                    externalizedName = FormatIdentifier(Current().Value[1..^1]);
-                }
-
-                ExternalName = externalizedName;
-            }
-
-            if (CurrentEquals("VALUE"))
-            {
-                Continue(1);
-                DataValue = Current().Value;
-            }
-
-            Continue(1);
-        }
-
-        static string ConvertType(ReadOnlySpan<char> current)
-        {
-            return current[0] switch
-            {
-                'X' => "Alphanumeric",
-                'A' => "Alphabetic",
-                'N' => "National",
-                '1' => "Bit",
-                '9' => "Numeric",
-                'S' => "Numeric",
-                _ => "Error"
-            };
-        }
-
-        if (Section == SourceScope.WorkingStorage && isElementary)
-        {
-            Compiled.Append($"private static {ConvertType(DataType)} {FormatIdentifier(Identifier)} = ");
-        }
-
-        if (Section == SourceScope.LocalStorage && isElementary)
-        {
-            Compiled.Append($"private {ConvertType(DataType)} {FormatIdentifier(Identifier)} = ");
-        }
-
-        string value;
-        int TotalLength = 0;
-
-        if (!DataType.Equals("9") && !DataType.Equals("S9"))
-        {
-            value = DataValue.Equals(String.Empty) ? "\" \"" : DataValue;
-            TotalLength = Length;
-
-            if (isElementary && isExternal)
-                Compiled.Append($"new(_{FormatIdentifier(Identifier)}.Memory, 0, {Length});");
-
-            if (isElementary && !isExternal)
-                Compiled.Append($"new({value}u8, 0, {Length}, new byte[{Length}]);");
-        }
-
-        if (DataType.Equals("9") || DataType.Equals("S9"))
-        {
-            value = DataValue.Equals(String.Empty) ? "\"0\"" : $"\"{DataValue}\"";
-
-            if (isSigned)
-            {
-                TotalLength = FractionalLength == 0 ? Length : Length + FractionalLength + 2;
-                if (value.IndexOfAny(new char[] { '+', '-' }) != 1) value = value.Insert(1, "+");
-
-                if (isElementary && isExternal)
-                    Compiled.Append($"new(_{FormatIdentifier(Identifier)}.Memory, 0, {Length}, {FractionalLength}, {isSigned.ToString().ToLower()});");
-
-                if (isElementary && !isExternal)
-                    Compiled.Append($"new({value}u8, 0, {Length}, {FractionalLength}, new byte[{TotalLength}]);");
-
-            }
-
-            if (!isSigned)
-            {
-                TotalLength = FractionalLength == 0 ? Length : Length + FractionalLength + 1;
-
-                if (isElementary && isExternal)
-                    Compiled.Append($"new(_{FormatIdentifier(Identifier)}.Memory, 0, {Length}, {FractionalLength}, {isSigned.ToString().ToLower()});");
-
-                if (isElementary && !isExternal)
-                    Compiled.Append($"new({value}u8, 0, {Length}, {FractionalLength}, new byte[{TotalLength}]);");
-            }
-        }
-
-        if (Section == SourceScope.WorkingStorage && isLevelOne && isExternal)
-            Compiled.Append($"""private static DataItem _{FormatIdentifier(Identifier)} = new(External.Resolver("{Identifier}", {TotalLength}));{Compiled}""");
-
-        if (Section == SourceScope.LocalStorage && isLevelOne && isExternal)
-            Compiled.Append($"""private DataItem _{FormatIdentifier(Identifier)} = new(External.Resolver("{Identifier}", {TotalLength}));{Compiled}""");
-
-        if (Section == SourceScope.WorkingStorage && isLevelOne && !isExternal)
-            Compiled.Append($"""private static DataItem _{FormatIdentifier(Identifier)} = new({TotalLength});{Compiled}""");
-
-        if (Section == SourceScope.LocalStorage && isLevelOne && !isExternal)
-            Compiled.Append($"""private DataItem _{FormatIdentifier(Identifier)} = new({TotalLength});{Compiled}""");
-
     }
 
     private void BuildConstant()
@@ -246,7 +105,7 @@ public class VariableBuilder
 
     }
 
-    private void Build77(DataEntry variable, Token token)
+    private void BuildVariable(DataEntry variable, Token token)
     {
         bool isSigned = false;
 
@@ -264,7 +123,7 @@ public class VariableBuilder
             _ => throw new ArgumentException("Invalid Type"),
         };
 
-        while (!CurrentEquals(".")) Continue(1);
+        while (!CurrentEquals(".")) Continue();
 
         var formatted = FormatIdentifier(token.Value);
 

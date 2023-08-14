83b5c5e3afb5fddb21f4699171e19147f062ae04
KTSnowy <KTSnowy@outlook.com>
2023-08-14 04:25:19 -0300

    [Update]: Refactor memcpy implementation

--- Libraries/Otterkit.Native/nativelib/memory.c
+++ Libraries/Otterkit.Native/nativelib/memory.c
@@ -1,12 +1,9 @@
 #include "CASPAL.h"
 
-typedef struct str
-{
-	uint8* Memory;
-	uint32 Length;
-} String;
+// Thin abstraction of a contiguous block of memory.
+typedef struct span_t { void* pointer; uint64 length; } MemorySpan;
 
-public void strAlignedZeroFill(const String to)
+public void ottrkAlignedZeroFill(const MemorySpan to)
 {
 	// Goto loop buffer index.
 	uint32 i = 0;
@@ -17,15 +14,15 @@ public void strAlignedZeroFill(const String to)
 	// Loop through the remaining bytes in the destination buffer, zeroing them out.
 	zeroLoop:
 	// 128-bit store of zeros to destination.
-	_mm_store_si128((vec128i*) to.Memory + i, zeros);
+	_mm_store_si128((vec128i*) to.pointer + i, zeros);
 
 	i += 16;
 
 	// If we haven't reached the end of the buffer, loop again.
-	if (i < to.Length) goto zeroLoop;
+	if (i < to.length) goto zeroLoop;
 }
 
-public void strAlignedFill(const uint8 with, const String to)
+public void ottrkAlignedFill(const uint8 with, const MemorySpan to)
 {
 	// Goto loop buffer index.
 	uint32 i = 0;
@@ -36,15 +33,15 @@ public void strAlignedFill(const uint8 with, const String to)
 	// Loop through the remaining bytes in the destination buffer, zeroing them out.
 	fillLoop:
 	// 128-bit store to destination.
-	_mm_store_si128((vec128i*) to.Memory + i, fill);
+	_mm_store_si128((vec128i*) to.pointer + i, fill);
 
 	i += 16;
 
 	// If we haven't reached the end of the buffer, loop again.
-	if (i < to.Length) goto fillLoop;
+	if (i < to.length) goto fillLoop;
 }
 
-public void strAlignedMove(const String from, const String to)
+public void ottrkAlignedMove(const MemorySpan from, const MemorySpan to)
 {
 	// Goto loop buffer index.
 	uint32 i = 0;
@@ -52,25 +49,25 @@ public void strAlignedMove(const String from, const String to)
 	// Loop through the source and destination buffers, copying 16 bytes at a time.
 	copyLoop:
 	// 128-bit load from source, 128-bit store to destination.
-	_mm_store_si128((vec128i*) to.Memory + i,
-		_mm_load_si128((vec128i const*) from.Memory + i)
+	_mm_store_si128((vec128i*) to.pointer + i,
+		_mm_load_si128((vec128i const*) from.pointer + i)
 	);
 
 	i += 16;
 
 	// If we haven't reached the end of the buffer, loop again.
-	if (i < from.Length && i < to.Length) goto copyLoop;
+	if (i < from.length && i < to.length) goto copyLoop;
 
 	// If we've reached the end of the destination buffer, no remaining bytes to zero out.
-	if (i >= to.Length) return;
+	if (i >= to.length) return;
 
 	// Loop through the remaining bytes in the destination buffer, zeroing them out.
 	zeroLoop:
 	// 128-bit store to destination.
-	_mm_store_si128((vec128i*) to.Memory + i, _mm_setzero_si128());
+	_mm_store_si128((vec128i*) to.pointer + i, _mm_setzero_si128());
 
 	i += 16;
 
 	// If we haven't reached the end of the buffer, loop again.
-	if (i < to.Length) goto zeroLoop;
+	if (i < to.length) goto zeroLoop;
 }

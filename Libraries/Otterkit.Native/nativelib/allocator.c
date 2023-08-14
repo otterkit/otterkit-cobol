3ae8b2833ea97034652c1a80adc6606824b59a0e
KTSnowy <KTSnowy@outlook.com>
2023-08-14 04:25:57 -0300

    [Update]: Create function to reserve virtual address space

--- Libraries/Otterkit.Native/nativelib/allocator.c
+++ Libraries/Otterkit.Native/nativelib/allocator.c
@@ -1,5 +1,22 @@
 #include "allocator.h"
 
+// Convert terabytes to bytes.
+#define TB(x) ((uint64)(x) * 1024 * 1024 * 1024 * 1024)
+
+private inline void* ottrkReserveAddressSpace(const uint64 size)
+{
+    // Reserve virtual address space, the default size should be 2 TB when calling this function.
+    #if defined(PlatformWindows)
+        return sysVirtualAlloc(nullptr, size, memProtected, MEM_RESERVE);
+
+    #elif defined(PlatformLinux) || defined(PlatformApple)
+        return sysVirtualAlloc(nullptr, size, memProtected, MAP_NORESERVE);
+
+    #endif
+}
+
+// TODO: Replace this with the proper allocator.
+
 // Convert megabytes to bytes.
 #define MB(x) ((uint32)(x) * 1024 * 1024)
 
@@ -10,7 +27,7 @@
 private uint8 Stack[MB(2)];
 
 // Stack pointer, points to the next available memory address.
-private uint8 ref StackPointer = Stack;
+private uint8* StackPointer = Stack;
 
 // Returns the amount of stack memory being used.
 // Might be useful for debugging or profiling.
@@ -31,7 +48,7 @@ public int32 StackAvailable()
 // A pointer to the allocated memory,
 // A NULL pointer if out of memory,
 // The current stack pointer if length is zero.
-public void ref Alloc(uint32 length)
+public void* Alloc(uint32 length)
 {
     // Return current stack pointer, if length is zero.
     if (length == 0) return StackPointer;
@@ -39,11 +56,11 @@ public void ref Alloc(uint32 length)
     // Return a null pointer, if there is not enough memory left on the stack.
     if (length > sizeof(Stack) - (StackPointer - Stack)) return NULL;
 
-    // Align the length to the next multiple of ALIGNMENT bytes.
-    length = ((length + ALIGNMENT) & -ALIGNMENT);
+    // Align the length to the next multiple of 16 bytes.
+    length = ((length + 16) & -16);
 
     // Get a pointer to the next available memory address.
-    void ref memory = StackPointer;
+    void* memory = StackPointer;
     
     // Increment stack pointer.
     StackPointer += length;
@@ -52,11 +69,11 @@ public void ref Alloc(uint32 length)
 }
 
 // Deallocate memory on the stack.
-public void Dealloc(void ref memory)
+public void Dealloc(void* memory)
 {
     // Return if memory is null.
     // Double free is not a good idea :)
-    if (memory == null) return;
+    if (memory == nullptr) return;
 
     // Reset stack pointer.
     // We don't need to zero out the memory, because it will be overwritten
--- Libraries/Otterkit.Native/nativelib/allocator.h
+++ Libraries/Otterkit.Native/nativelib/allocator.h
@@ -1,7 +1,7 @@
 #if !defined(OTTERKIT_ALLOCATOR_H)
 #define OTTERKIT_ALLOCATOR_H
 
-#include "common.h"
+#include "CASPAL.h"
 
 // Returns the amount of stack memory being used.
 // Might be useful for debugging or profiling.
@@ -16,9 +16,9 @@ int32 StackAvailable();
 // A pointer to the allocated memory,
 // A NULL pointer if out of memory,
 // The current stack pointer if length is zero.
-void *Alloc(uint32 length);
+void* Alloc(uint32 length);
 
 // Deallocate memory on the stack.
-void Dealloc(void *memory);
+void Dealloc(void* memory);
 
 #endif // OTTERKIT_ALLOCATOR_H

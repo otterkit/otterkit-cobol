5538733c6564f5dd37295de5ba2b5b434df4b7cd
KTSnowy <KTSnowy@outlook.com>
2023-08-29 07:41:27 -0300

    Update allocator.c

--- Libraries/Otterkit.Native/nativelib/allocator.c
+++ Libraries/Otterkit.Native/nativelib/allocator.c
@@ -1,305 +1,358 @@
-#include "CASPAL.h"
 #include <stdio.h>
+#include "allocator.h"
 
-typedef struct vpool_t
+private VirtualHeap GlobalVirtualHeap;
+
+void VirtualHeapInitialize(VirtualHeap* instance, uint64 size)
 {
-    uint16 Capacity;
-    uint16 Available;
-    uint16 Initialized;
-    uint16 BlockLength;
-    uint8* NextBlock;
-} VirtualPool;
-
-typedef struct vcache_t
-{
-    // Immediate: directly available for allocation.
-    VirtualPool* Immediate;
-
-    // Cached: used when present, instead of creating a new pool.
-    VirtualPool* Cached;
-} VirtualCache;
-
-// Bit tree of available virtual pools.
-typedef struct vtree_t
-{
-    // 64 bits (1 bit per 2 TiB)
-    uint64 Root;
-    
-    // 4096 bits (1 bit per 32 GiB)
-    uint64 Trunks[1 << 6];
-
-    // 131072 bits (1 bit per 1 GiB)
-    uint64 Branches[1 << 12];
-
-    // 4194304 bits (1 bit per 32 MiB)
-    uint64 Twigs[1 << 18];
-
-    // 1073741824 bits (1 bit per 128 KiB)
-    uint64 Leaves[1 << 24];
-} VirtualTree;
-
-typedef struct vallocator_t
-{
-    // Allocator's base virtual address space.
-    uint8* BaseAddress;
-
-    // This instance's capacity, in bytes (max 128 TiB).
-    uint64 Capacity;
-
-    // This instance's current usage, in bytes.
-    uint64 Usage;
-
-    // This instance's immediate pool lookup table and pool cache.
-    // At least one pool is always kept in the cache after initialization.
-    VirtualCache HeapCache[120];
-
-    // This instance's bit tree, used to track available virtual pools.
-    VirtualTree* BitTree;
-
-    uint8 IsInitialized;
-} VirtualAllocator;
-
-static VirtualAllocator GlobalAllocator;
-
-void VAllocInitialize(uint64 size)
-{
-    // Avoid overwriting the allocator if it has already been initialized.
-    if (GlobalAllocator.IsInitialized) return;
+    // Avoid overwriting the allocator instance if it has already been initialized.
+    if (instance->IsInitialized) return;
 
     // If the size is 0, then we allocate the default 2TiB of address space.
     if (size == 0) size = 2ull << 40;
 
-    // Reserve the heap's address space, and an additional 128KiB for alignment purposes.
-    void* heap = sysReserveAddressSpace(size + 131072);
-
-    // (void*)-1 is MAP_FAILED, expanded to avoid the check breaking on platforms where it is not defined.
-    if (heap == nullptr || heap == (void*)-1)
-    {
-        fputs("Failed to reserve address space for the heap allocator.\n", stderr);
-        exit(1);
-    }
-
-    // Commit the first 2MiB of the heap's address space.
-    sysCommitMemory(heap, 2097152);
+    // Reserve the allocator's address space, and an additional 128KiB for alignment purposes.
+    void* base = SystemAlloc(nullptr, size + 131072, SYS_PROTECTED, SYS_RESERVE);
 
     // Align the heap to a 128KiB boundary (needed to make deallocation work properly)
-    uintptr aligned = ((uintptr)heap + (131072 - 1)) & -131072;
+    uintptr aligned = ((uintptr)base + (131072 - 1)) & -131072;
 
-    GlobalAllocator.BaseAddress = (uint8*)aligned;
+    instance->BaseAddress = (uint8*)aligned;
+    instance->Capacity = size;
 
-    // Allocate 4MiB of memory for the virtual allocator header.
-    uint8* headerAddress = sysVirtualAlloc(nullptr, 4194304, memReadWrite, memAllocate);
-
-    uint64 index = 0;
-    uint64 segmentSize = size / 16;
-    
-    // Initialize all 16 virtual segments.
-    while (GlobalAllocator.Segments[15] == nullptr)
-    {
-        VirtualSegment** segment = &GlobalAllocator.Segments[index];
-
-        *segment = (VirtualSegment*)(headerAddress + 163840ull * index);
-
-        (*segment)->Memory = headerAddress + segmentSize * index;
-
-        (*segment)->Available = segmentSize;
-        (*segment)->Capacity = segmentSize;
-
-        index++;
-    }
-}
-
-// initializer void virtAllocatorInitializeDefault() { virtAllocatorInitialize(0); }
-
-VirtualAllocator* VAllocFetchGlobalInstance() { return &GlobalAllocator; }
-
-static void* FetchAvailableAddress(VirtualAllocator* instance)
-{
-    VirtualTree* tree = instance->BitTree;
-
-    uint64* root = &tree->Root;
-
-    uint64 rootIndex = __tzcnt_u64(~(*root)); // 64
-
-    uint64* trunk = &tree->Trunks[rootIndex];
-
-    uint64 trunkIndex = __tzcnt_u64(~(*trunk)); // 4096
-
-    uint64* branch = &tree->Branches[
-        (rootIndex << 6) + trunkIndex
-    ];
-
-    uint64 branchIndex = __tzcnt_u64(~(*branch)); // 131072
-
-    uint64* twig = &tree->Twigs[
-        (rootIndex << 12) + (trunkIndex << 6) + branchIndex
-    ];
-
-    uint64 twigIndex = __tzcnt_u64(~(*twig)); // 4194304
-
-    uint64* leaf = &tree->Leaves[
-        (rootIndex << 18) + (trunkIndex << 12) + (branchIndex << 6) + twigIndex
-    ];
-
-    uint64 leafIndex = __tzcnt_u64(~(*leaf)); // 1073741824
-
-    *leaf |= (1u << leafIndex);
-
-    if (*leaf == ~0u) *branch |= (1u << branchIndex);
-
-    if (*branch == ~0u) *trunk |= (1u << trunkIndex);
-
-    if (*trunk == ~0u) *root |= (1u << rootIndex);
-
-    // If the root is full, then the entire segment is full (128GiB)
-    // if (*root == ~0u) return nullptr;
-
-    return (
-        instance->BaseAddress
-        + (rootIndex << 24)
-        + (trunkIndex << 18)
-        + (branchIndex << 12)
-        + (twigIndex << 6)
-        + leafIndex
+    // Allocate the bit tree.
+    instance->BitTree = (VirtualTree*)SystemAlloc(
+        nullptr, sizeof(VirtualTree), SYS_READWRITE, SYS_ALLOCATE
     );
 }
 
-// void virtPoolRelease(VirtualSegment* segment, VirtualPool* pool)
-// {
-//     uint32* root = &segment->Root;
+initializer void VirtualHeapInitializeDefault() { VirtualHeapInitialize(&GlobalVirtualHeap, 0); }
 
-//     uint32 rootIndex = (
-//         ((uintptr)pool - (uintptr)segment->Memory) >> 15
-//     );
+VirtualHeap* VirtualHeapGetGlobalInstance() { return &GlobalVirtualHeap; }
 
-//     uint32* trunk = &segment->Trunks[
-//         rootIndex
-//     ];
+void VirtualPoolInitialize(VirtualPool* pool, uint64 blockLength)
+{
+    // Initialize the pool. Pool capacity is 128KiB / blockLength.
+    pool->Capacity = 131072 / blockLength;
+    pool->Available = pool->Capacity;
+    pool->Initialized = 0;
+    pool->BlockLength = blockLength;
+    pool->NextBlock = 0;
+}
 
-//     uint32 trunkIndex = (
-//         ((uintptr)pool - (uintptr)segment->Memory) >> 10
-//     ) & ((1 << 5) - 1);
+VirtualPool* VirtualPoolCreate(VirtualHeap* allocator, uint64 blockLength)
+{
+    VirtualTree* tree = allocator->BitTree;
 
-//     uint32* branch = &segment->Branches[
-//         (rootIndex << 5) + trunkIndex
-//     ];
+    uint64* root = &tree->Root;
 
-//     uint32 branchIndex = (
-//         ((uintptr)pool - (uintptr)segment->Memory) >> 5
-//     ) & ((1 << 10) - 1);
+    uint64 indexRoot = __tzcnt_u64(~(*root)); // 64
 
-//     uint32* leaf = &segment->Leaves[
-//         (rootIndex << 10) + (trunkIndex << 5) + branchIndex
-//     ];
+    uint64* trunk = &tree->Trunks[indexRoot];
 
-//     uint32 leafIndex = (
-//         ((uintptr)pool - (uintptr)segment->Memory)
-//     ) & ((1 << 15) - 1);
+    uint64 indexTrunk = __tzcnt_u64(~(*trunk)); // 4096
 
-//     *leaf &= ~(1u << leafIndex);
+    uint64* branch = &tree->Branches[(indexRoot << 6) + indexTrunk];
 
-//     if (*leaf == 0u) *branch &= ~(1u << branchIndex);
+    uint64 indexBranch = __tzcnt_u64(~(*branch)); // 131072
 
-//     if (*branch == 0u) *trunk &= ~(1u << trunkIndex);
+    uint64* twig = &tree->Twigs[(indexRoot << 12) + (indexTrunk << 6) + indexBranch];
 
-//     if (*trunk == 0u) *root &= ~(1u << rootIndex);
+    uint64 indexTwig = __tzcnt_u64(~(*twig)); // 4194304
 
-//     // Decommit the pool's pages (128KiB), still keep it reserved.
-//     sysDecommitMemory(pool, 131072);
-// }
+    uint64* leaf = &tree->Leaves[(indexRoot << 18) + (indexTrunk << 12) + (indexBranch << 6) + indexTwig];
+
+    uint64 indexLeaf = __tzcnt_u64(~(*leaf)); // 1073741824
+
+    // Check for invalid pool allocation.
+    // if (*leaf & (1ull << indexLeaf))
+    // {
+    //     printf("Invalid pool allocation!\n");
+    //     return nullptr;
+    // }
+
+    // printf("Allocating pool at %llu %llu %llu %llu %llu\n", indexRoot, indexTrunk, indexBranch, indexTwig, indexLeaf);
+
+    *leaf |= (1ull << indexLeaf);
+
+    if (*leaf == ~0ull) *twig |= (1ull << indexTwig);
+
+    if (*twig == ~0ull) *branch |= (1ull << indexBranch);
+
+    if (*branch == ~0ull) *trunk |= (1ull << indexTrunk);
+
+    if (*trunk == ~0ull) *root |= (1ull << indexRoot);
+
+    // If the root is full, then the entire heap is full (all 128TiB of it)
+    if (*root == ~0ull) return nullptr;
+
+    VirtualPool* pool = ComputePoolAddress(
+        allocator->BaseAddress, indexRoot, indexTrunk, indexBranch, indexTwig, indexLeaf
+    );
+
+    // Commit the pool's address space.
+    pool = SystemCommit(pool, 131072);
+
+    // Initialize the virtual pool.
+    VirtualPoolInitialize(pool, blockLength);
+
+    // Store the pool's bit tree index.
+    pool->Index = PackVirtualIndex(
+        indexRoot, indexTrunk, indexBranch, indexTwig, indexLeaf
+    );
+
+    return pool;
+}
+
+void VirtualPoolDecommit(VirtualHeap* allocator, VirtualPool* pool)
+{
+    VirtualIndex index = pool->Index;
+
+    UnpackVirtualIndex(index);
+
+    VirtualTree* tree = allocator->BitTree;
+
+    uint64* root = &tree->Root;
+
+    uint64* trunk = &tree->Trunks[indexRoot];
+
+    uint64* branch = &tree->Branches[
+        (indexRoot << 6) + indexTrunk
+    ];
+
+    uint64* twig = &tree->Twigs[
+        (indexRoot << 12) + (indexTrunk << 6) + indexBranch
+    ];
+
+    uint64* leaf = &tree->Leaves[
+        (indexRoot << 18) + (indexTrunk << 12) + (indexBranch << 6) + indexTwig
+    ];
+
+    // Check for invalid pool deallocation.
+    // if (!(*leaf & (1ull << indexLeaf)))
+    // {
+    //     printf("Invalid pool deallocation!\n");
+    //     return;
+    // }
+
+    // printf("Deallocating pool at %llu %llu %llu %llu %llu\n", indexRoot, indexTrunk, indexBranch, indexTwig, indexLeaf);
+
+    *leaf &= ~(1ull << indexLeaf);
+
+    if (*leaf == 0ull) *twig &= ~(1ull << indexTwig);
+
+    if (*twig == 0ull) *branch &= ~(1ull << indexBranch);
+
+    if (*branch == 0ull) *trunk &= ~(1ull << indexTrunk);
+
+    if (*trunk == 0ull) *root &= ~(1ull << indexRoot);
+
+    // Decommit the pool's address space.
+    SystemDecommit(pool, 131072);
+}
 
 // Apparently, GCC can't optimize this function properly and generates a bunch of branches.
 // This is a performance-critical function that is called every time a block is allocated.
-// We can't afford to have it be slow due to branch mispredictions.
-AssemblyFunction(uint64, AlignLengthToPool, uint64 length)
+// We can't afford to have it be slow due to branch mispredictions, please compile with Clang.
+uint64 AlignLengthToSizeClass(uint64 length)
+{
+    // rdi = length (sysv calling convention)
+    uint64 aligned;
 
-#if defined AMD64
-Assembly (
-    UsingIntelSyntax
-    "AlignLengthToPool:         \n"
-    "test    rcx, rcx           \n"
-    "mov     rdx, 1             \n"
-    "cmovne  rdx, rcx           \n"
-    "lea     rcx, [rdx + 127]   \n"
-    "lea     rax, [rdx + 15]    \n"
-    "shr     rcx, 7             \n"
-    "shr     rax, 4             \n"
-    "add     rcx, 56            \n"
-    "cmp     rdx, 1025          \n"
-    "cmovae  rax, rcx           \n"
-    "dec     rax                \n"
-    "ret                        \n"
-);
-#elif defined ARM64
-Assembly (
-    "AlignLengthToPool:         \n"
-    "cmp    x0,  #0             \n"
-    "csinc  x8,  x0,  xzr, ne   \n"
-    "add    x9,  x8,  #127      \n"
-    "add    x10, x8,  #15       \n"
-    "lsr    x9,  x9,  #7        \n"
-    "lsr    x10, x10, #4        \n"
-    "add    x9,  x9,  #56       \n"
-    "cmp    x8,  #1025          \n"
-    "csel   x8,  x10, x9,  lo   \n"
-    "sub    x0,  x8,  #1        \n"
-    "ret                        \n"
-);
-#endif
+    // test    rdi, rdi
+    // mov     rcx, 1
+    // cmovne  rcx, rdi
+    if (length == 0) length = 1;
+
+    // lea     rdx, [rcx + 127]
+    // shr     rdx, 7
+    // add     rdx, 56
+    aligned = length + (128 - 1);
+    aligned = (aligned >> 7) + 56;
+
+    // lea     rax, [rcx + 15]
+    // shr     rax, 4
+    // cmp     rcx, 1025
+    // cmovae  rax, rdx
+    if (length <= 1024)
+    {
+        aligned = length + (16 - 1);
+        aligned = aligned >> 4;
+    }
+
+    // dec     rax
+    // ret
+    return aligned - 1;
+}
 
 int main(void)
 {
-    for (uint64 i = 0; i < 8192; i += 16)
-    {
-        uint64 class = AlignLengthToClass(i);
-
-        printf("Size: %llu, Class: %llu\n", i, class);
-    }
-
     return 0;
 }
 
-// void* ottrkPoolAlloc(VirtualPool* pool)
-// {
-//     // If there are any uninitialized blocks, initialize one.
-//     if (pool->Initialized < pool->Capacity)
-//     {
-//         // Get the address of the next uninitialized block.
-//         uint8* uninitialized = (
-//             (uint8*)pool + 32 + pool->Initialized * pool->BlockLength
-//         );
-        
-//         // Set the next block pointer to the next available block.
-//         *(uint8**)uninitialized = uninitialized + pool->BlockLength;
+void* VirtualPoolAllocateSmall(VirtualPool* pool)
+{
+    uint8* block = nullptr;
 
-//         pool->Initialized++;
+    // If there are any uninitialized blocks, initialize one.
+    if (pool->Initialized != pool->Capacity)
+    {
+        // Get the index of the uninitialized block.
+        uint16 index = pool->Initialized++;
+        // Get the address of the uninitialized block.
+        uint8* block = (uint8*)pool + 16 + index * pool->BlockLength;
+        // Set the next block index to the next uninitialized block.
+        *(uint16*)block = index + 1;
+    }
+
+    // If there are any available blocks, allocate one.
+    if (pool->Available != 0)
+    {
+        // Get the address of the next available block.
+        block = (uint8*)pool + 16 + pool->NextBlock * pool->BlockLength;
+        // Decrement the number of available blocks.
+        pool->Available--;
+        // If there are any available blocks left, set 
+        // the next block index to the next available block.
+        pool->NextBlock = pool->Available ? *(uint16*)block : 0;
+    }
+
+    return block;
+}
+
+void* VirtualHeapAllocateSmall(uint64 length)
+{
+    // Get the global allocator instance.
+    VirtualHeap* allocator = VirtualHeapGetGlobalInstance();
+    // Align the length to one of the small size classes.
+    uint64 class = AlignLengthToSizeClass(length);
+    // Fetch the allocator's immediate pool for the size class.
+    VirtualPool* pool = allocator->Immediate[class];
+
+    // Allocate a block from the pool if it's available.
+    if (pool != nullptr)
+    {
+        // Allocate a block from the pool.
+        void* block = VirtualPoolAllocateSmall(pool);
+        // If the pool is now empty, set the immediate pool to null.
+        if (pool->Available == 0)
+        {
+            allocator->Immediate[class] = nullptr;
+        }
+
+        return block;
+    }
+
+    // If an immediate pool is not available, allocate a new one.
+    pool = VirtualPoolCreate(allocator, length);
+
+    // If a new pool was successfully allocated
+    if (pool != nullptr)
+    {
+        // Set it as the immediate pool for the size class.
+        allocator->Immediate[class] = pool;
+        // Allocate a block from the new pool.
+        return VirtualPoolAllocateSmall(pool);
+    }
+
+    // Most likely, the heap is full.
+    return nullptr;
+}
+
+void VirtualPoolDeallocateSmall(VirtualPool* pool, void* address)
+{
+    // Set the deallocated block's next block index to 
+    // the next available block.
+    *(uint16*)address = pool->NextBlock;
+    // Get the deallocated block's absolute index.
+    uint16 index = (uint8*)address - (uint8*)pool - 16;
+    // Set the next available block to the deallocated 
+    // block's relative index.
+    pool->NextBlock = index / pool->BlockLength;
+    // Increment the number of available blocks.
+    pool->Available++;
+}
+
+void VirtualHeapDeallocateSmall(void* address)
+{
+    // Get the global allocator instance.
+    VirtualHeap* allocator = VirtualHeapGetGlobalInstance();
+    
+    // Get the pool that the deallocated block belongs to.
+    VirtualPool* pool = (VirtualPool*)((uintptr)address & -131072);
+
+    // Deallocate the block from the pool.
+    VirtualPoolDeallocateSmall(pool, address);
+
+    uint64 class = AlignLengthToSizeClass(pool->BlockLength);
+
+    // Get the immediate pool for the size class.
+    VirtualPool** immediate = &allocator->Immediate[class];
+
+    // Set the pool as the immediate pool for the size class, if there isn't one already.
+    if (*immediate == nullptr) *immediate = pool;
+
+    // Decommit the pool if it's empty and there's already an immediate pool for the size class.
+    if (pool->Available == pool->Capacity && *immediate != nullptr)
+    {
+        VirtualPoolDecommit(allocator, pool);
+    }
+}
+
+// void AllocatorTest(void)
+// {
+//     VirtualHeap* allocator = VirtualHeapGetGlobalInstance();
+
+//     void* prev = nullptr;
+//     uint64 count = 0;
+
+//     for (uint64 i = 0; i < allocator->Capacity; i += (1ull << 17))
+//     {
+//         void* address = AllocatorCreateVirtualPool(allocator, 16);
+
+//         // Test if we ran out of memory earlier than expected.
+//         // This should never happen during this test (in practice, it will happen when the heap is full)
+//         if (address == nullptr)
+//         {
+//             printf("Out of memory too early!\n");
+//             break;
+//         }
+
+//         // Test if the address is not contiguous with the previous one.
+//         // This error should never happen, otherwise the allocator indexing is broken.
+//         if (prev != nullptr && (uintptr)address != (uintptr)prev + (1ull << 17))
+//         {
+//             printf("Address mismatch! %p %p %llu\n", prev, address, (uintptr)address - (uintptr)prev);
+//             break;
+//         }
+
+//         // Test if the address is not aligned to a 128KiB boundary.
+//         // This error should never happen, otherwise the allocator initializer is broken.
+//         if ((uintptr)address & (131072 - 1))
+//         {
+//             printf("Address not aligned! %p\n", address);
+//             break;
+//         }
+
+//         count++;
+//         prev = address;
 //     }
 
-//     uint8* block = nullptr;
-
-//     // If there are any available blocks, allocate one.
-//     if (pool->Available)
+//     if ((uintptr)prev + 131072 > (uintptr)allocator->BaseAddress + allocator->Capacity)
 //     {
-//         block = pool->NextBlock;
-//         pool->Available--;
-
-//         // If there are any available blocks left, set the next block pointer to the next available block.
-//         pool->NextBlock = (pool->Available) ? *(uint8**)block : nullptr;
+//         printf("Address out of bounds! %p\n", prev);
 //     }
 
-//     return block;
-// }
+//     printf("Allocated %llu blocks\n", count);
 
-// void ottrkPoolDealloc(VirtualPool* pool, void* address)
-// {
-//     // Set the next block pointer to the pool's next available block.
-//     *(uint8**)address = pool->NextBlock;
+//     // Test if the allocator can release addresses properly.
+//     for (uint64 i = 0; i < allocator->Capacity; i += (1ull << 17))
+//     {
+//         AllocatorDecommitVirtualPool(allocator, prev);
+//         prev = (uint8*)prev - (1ull << 17);
 
-//     // Set the deallocated block as the next available block.
-//     pool->NextBlock = address;
+//         count--;
+//     }
 
-//     pool->Available++;
-
-//     // If the pool is empty, free it.
-//     if (pool->Available == pool->Capacity) virtPoolRelease(pool); 
+//     if (count != 0) printf("Failed to release all addresses! %llu\n", count);
 // }
